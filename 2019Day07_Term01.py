51f82d2c60bdd2992872fe61d2e6dec6bcc64afd
fly_saint_in_home <1357810clock@163.com>
2019-03-20 00:15:47 +0800

    更新day07

--- 2019Day07_Term01.py
+++ 2019Day07_Term01.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Mar  9 23:47:43 2019
-
 @author: fly_s
-
 Day07
 1. 来排个序
 对给定字符串排序。字符串中每个单词都包含一个数字。此数字是单词在在结果中应该具有位置。
@@ -48,6 +46,60 @@ def extract_number(word):
     return None
 def order(sentence):
     return ' '.join(sorted(sentence.split(), key=extract_number))
+'''
+2 点赞
+你可能知道Facebook和其他网页上的“点赞”系统。人们可以“喜欢”博客文章，图片或其他项目。
+我们想要创建应该在这样的项目旁边显示的文本。
+实现一个函数
+likes :: [String] --> String, 它必须包含输入数组，包含喜欢项目的人的名字
+它必须返回显示文本，如示例所示：
+likes([]) // must be "no one likes this"
+likes(["Peter"]) // must be "Peter likes this"
+likes(["Jacob", "Alex"]) // must be "Jacob and Alex like this"
+likes(["Max", "John", "Mark"]) // must be "Max, John and Mark like this"
+likes(["Alex", "Jacob", "Mark", "Max"]) // must be "Alex, Jacob and 2 others
+like this"
+对于4个或更多名称，数字 and 2 others 只会增加
+点评
+1. 利用字典来返回字符串结构，非常巧妙。返回value，返回各种字符串
+2. 字典的遍历利用 min(4,n)
+3. format里面 关键字定位，非常易扩展
+4. *['aa'] 星号和列表连用的做法很独特
+'''
+# 高手法1
+def likes(names):
+    n = len(names)
+    return {
+    0: 'no one likes this',
+    1: '{} likes this',
+    2: '{} and {} like this',
+    3: '{}, {} and {} like this',
+    4: '{}, {} and {others} others like this'
+    '''
+    这里的 *names[:3] 是将name[:3]放入一个元组里，这样前面的 3个{}都会有值
+    * 代表 starred expression 详见 https://blog.csdn.net/DawnRanger/article/details/78028171
+    # example 1
+    a, *b, c = range(5)
+    此时: a = 0,*b = [1, 2, 3],c = 4
+    '''
+    }[min(4, n)].format(*names[:3], others=n-2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
